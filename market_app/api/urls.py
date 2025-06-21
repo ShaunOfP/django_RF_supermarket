18a688f92b3903644e94db3f5b624ae8413b1ad3
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2025-06-21 13:11:31 +0200

    replaced all class views with ModelViewSets

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
-from .views import MarketsView, MarketSingleView, SellerOfMarketList, SellersView, SellerSingleView, ProductsView
+from django.urls import path, include
+from .views import SellerOfMarketList, MarketViewSet, SellerViewSet, ProductViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'market', MarketViewSet)
+router.register(r'seller', SellerViewSet)
+router.register(r'product', ProductViewSet)
+
+
 
 urlpatterns = [
-    path('market/', MarketsView.as_view()),
-    path('market/<int:pk>/', MarketSingleView.as_view(), name='market-detail'),
+    path('', include(router.urls)),
+    # path('market/', MarketsView.as_view()),
+    # path('market/<int:pk>/', MarketSingleView.as_view(), name='market-detail'),
     path('market/<int:pk>/sellers/', SellerOfMarketList.as_view()),
-    path('seller/', SellersView.as_view()),
-    path('seller/<int:pk>/', SellerSingleView.as_view(), name='seller-detail'),
-    path('product/', ProductsView.as_view())
+    # path('seller/', SellersView.as_view()),
+    # path('seller/<int:pk>/', SellerSingleView.as_view(), name='seller-detail'),
 ]
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -2,21 +2,26 @@ from rest_framework.response import Response
 from .serializers import MarketSerializer, SellerSerializer, ProductSerializer, SellerListSerializer
 from market_app.models import Market, Seller, Product
 from rest_framework.views import APIView
-from rest_framework import mixins
-from rest_framework import generics
+from rest_framework import mixins, generics, viewsets
+from django.shortcuts import get_object_or_404
 
 
-class MarketsView(generics.ListCreateAPIView):
+# class MarketsView(generics.ListCreateAPIView):
+#     queryset = Market.objects.all()
+#     serializer_class = MarketSerializer
+
+
+# class MarketSingleView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Market.objects.all()
+#     serializer_class = MarketSerializer
+
+
+class MarketViewSet(viewsets.ModelViewSet):
     queryset = Market.objects.all()
     serializer_class = MarketSerializer
 
 
-class MarketSingleView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Market.objects.all()
-    serializer_class = MarketSerializer
-
-
-class SellerOfMarketList(generics.ListAPIView):
+class SellerOfMarketList(generics.ListCreateAPIView):
     serializer_class = SellerListSerializer
 
     def get_queryset(self):
@@ -24,17 +29,61 @@ class SellerOfMarketList(generics.ListAPIView):
         market = Market.objects.get(pk=pk)
         return market.sellers.all()
 
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        market = Market.objects.get(pk=pk)
+        serializer.save(markets=[market])
 
-class SellersView(generics.ListCreateAPIView):
+
+# class SellersView(generics.ListCreateAPIView):
+#     queryset = Seller.objects.all()
+#     serializer_class = SellerSerializer
+
+
+# class SellerSingleView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Seller.objects.all()
+#     serializer_class = SellerSerializer
+
+
+class SellerViewSet(viewsets.ModelViewSet):
     queryset = Seller.objects.all()
     serializer_class = SellerSerializer
 
 
-class SellerSingleView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Seller.objects.all()
-    serializer_class = SellerSerializer
+# class ProductsView(generics.ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
 
-class ProductsView(generics.ListCreateAPIView):
+# class ProductViewSetOld(viewsets.ViewSet):
+#     queryset = Product.objects.all()
+
+#     def list(self, request):
+#         serializer = ProductSerializer(self.queryset, many=True)
+#         return Response(serializer.data)
+
+#     def retrieve(self, request, pk=None):
+#         product = get_object_or_404(self.queryset, pk=pk)
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)
+
+#     def create(self, request):
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
+#     def destroy(self, request, pk=None):
+#         product = get_object_or_404(self.queryset, pk=pk)
+#         serializer = ProductSerializer(product)
+#         product.delete()
+#         return Response(serializer.data)
+#  wurde ersetzt durch ModelViewSet
+
+
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    #ModelViewSet vereint z.B. MarketsView und MarketSingleView in einer Class
